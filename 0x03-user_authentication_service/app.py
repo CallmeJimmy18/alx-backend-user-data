8e814ab8636e462a90639f6787266f644fd1bcdb
Gman <matolagarnet@gmail.com>
2024-04-25 08:25:21 -0700

    Project files created

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -100,17 +100,13 @@ def update_password() -> str:
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
-    password_changed = False
+    
     try:
         AUTH.update_password(reset_token, new_password)
-        password_changed = True
-    except ValueError:
-        password_changed = False
-
-    if not password_changed:
+    except Exception::
         abort(403)
 
-    return jsonify({"email": email, "message": "Password updated"})
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
